1a5205b359eaba6daed898cdbf499c8a40d27efd
Hugo Corra <hugocorra@gmail.com>
2019-03-21 21:40:40 -0300

    added args to specify the intervals

--- beeper.py
+++ beeper.py
@@ -1,8 +1,22 @@
+import argparse
 import time
 from playsound import playsound
 
+default_sound = 'Airplane-ding-sound.mp3'
+
+def play_default():
+    playsound(default_sound)
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Beep each N seconds.')
+    parser.add_argument('seconds', metavar='N', type=int, nargs='?', default=15,
+                       help='interval (in seconds) between each beep')
+
+    args = parser.parse_args()
+    seconds = args.seconds
+
+    play_default()
     while (True):
-        time.sleep(5)
-        playsound('Airplane-ding-sound.mp3')
+        time.sleep(seconds)
+        play_default()
