36a170e45bf7c77b0d76478673c219bde6c3b667
Hugo Corra <hugocorra@gmail.com>
2019-07-24 20:30:45 -0300

    modularized timer panel parts

--- beeperweb.py
+++ beeperweb.py
@@ -2,9 +2,22 @@ from bottle import TEMPLATE_PATH, jinja2_view, route, run, template
 
 TEMPLATE_PATH[:] = ['templates']
 
+
 @route('/')
 @jinja2_view('index.html')
 def index():
     return {'title': 'Hello world'}
+
+
+@route('/newpanel/<name>')
+@jinja2_view('panel.html')
+def newpanel(name):
+    return {'name': 'hugo'}
+
+
+@route('/newtimer/<panel>/<id>')
+@jinja2_view('timer.html')
+def newtimer(panel, id):
+    return {}
 
 run(host='localhost', port=8080, debug=True)
