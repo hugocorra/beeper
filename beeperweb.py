261f6a93b33f3afa572634838a4d9316da32168e
Hugo Corra <hugocorra@gmail.com>
2019-08-07 06:07:53 -0300

    server providing files

--- beeperweb.py
+++ beeperweb.py
@@ -1,23 +1,15 @@
-from bottle import TEMPLATE_PATH, jinja2_view, route, run, template
+from bottle import TEMPLATE_PATH, jinja2_view, route, run, template, static_file
 
 TEMPLATE_PATH[:] = ['templates']
 
 
 @route('/')
-@jinja2_view('index.html')
+@jinja2_view('index-vue.html')
 def index():
-    return {'title': 'Hello world'}
-
-
-@route('/newpanel/<name>')
-@jinja2_view('panel.html')
-def newpanel(name):
-    return {'name': 'hugo'}
-
-
-@route('/newtimer/<panel>/<id>')
-@jinja2_view('timer.html')
-def newtimer(panel, id):
     return {}
+
+@route('/static/<path:path>')
+def static_files(path):
+    return static_file(path, 'static')
 
 run(host='localhost', port=8080, debug=True)
