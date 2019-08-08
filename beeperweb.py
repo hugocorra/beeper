a197f456dc29b18800c66643c58a56233278e2ef
Hugo Corra <hugocorra@gmail.com>
2019-08-08 06:13:37 -0300

    implemented save/load apis

--- beeperweb.py
+++ beeperweb.py
@@ -1,3 +1,4 @@
+import json
 from bottle import TEMPLATE_PATH, jinja2_view, request, route, run, template, static_file
 
 TEMPLATE_PATH[:] = ['templates']
@@ -11,9 +12,25 @@ def index():
 
 @route('/save', method='POST')
 def save():
-    #from IPython import embed;embed()
-    #print(request.json)
-    return {}
+    try:
+        #from IPython import embed; embed()
+        with open('mydb.txt', 'w') as db:
+            json.dump(request.json, db)
+    except Exception as e:
+        return {'status': 'NOK', 'what': str(e)}
+
+    return {'satatus': 'OK'}
+
+
+@route('/load', method='GET')
+def load():
+    try:
+        with open('mydb.txt') as db:
+            json_contents = json.load(db)
+    except Exception as e:
+        return {'status': 'NOK', 'what': str(e)}
+
+    return {'status': 'OK', 'data': json_contents }
 
 
 @route('/static/<path:path>')
