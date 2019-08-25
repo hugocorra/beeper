32d3aa79e1a5f62eba67125b69ed014a16ad3b01
Hugo Corra <hugocorra@gmail.com>
2019-08-25 10:37:36 -0300

    remove invalid code

--- beeperweb.py
+++ beeperweb.py
@@ -1,11 +1,7 @@
 import json
 import os
 
-from flask import Flask, render_template, abort, redirect, url_for, session
-# from flask_oauth import OAuth
-# from google_auth import login as glogin
-# from google_auth import logout as glogout
-
+from flask import Flask, render_template, abort, redirect, url_for, session, request
 import google_auth
 
 
@@ -13,52 +9,6 @@ app = Flask(__name__, static_url_path='/static')
 app.secret_key = os.environ.get("FN_FLASK_SECRET_KEY")
 
 app.register_blueprint(google_auth.app)
-
-# REDIRECT_URI = '/authorized'
-# GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-# GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-# GOOGLE_DISCOVERY_URL = (
-#     "https://accounts.google.com/.well-known/openid-configuration"
-# )
-
-
-
-#TEMPLATE_PATH[:] = ['templates']
-
-
-# @app.route('/login')
-# #@jinja2_view('login.html')
-# def login():
-#     return {}
-
-
-
-
-########################
-
-
-# app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
-
-# # User session management setup
-# # https://flask-login.readthedocs.io/en/latest
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
-# # Naive database setup
-# try:
-#     init_db_command()
-# except sqlite3.OperationalError:
-#     # Assume it's already been created
-#     pass
-
-# # OAuth 2 client setup
-# client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
-# # Flask-Login helper to retrieve a user from our db
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.get(user_id)
-
 
 #########################
 
@@ -70,23 +20,9 @@ def login_callback():
 @app.route("/login")
 def login():
     return render_template('login2.html')
-    # # Find out what URL to hit for Google login
-    # google_provider_cfg = get_google_provider_cfg()
-    # authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-    # # Use library to construct the request for Google login and provide
-    # # scopes that let you retrieve user's profile from Google
-    # request_uri = client.prepare_request_uri(
-    #     authorization_endpoint,
-    #     redirect_uri=request.base_url + "/callback",
-    #     scope=["openid", "email", "profile"],
-    # )
-    # return redirect(request_uri)
-
 
 
 @app.route('/')
-#@jinja2_view('index-vue.html')
 def index():
     return render_template('index-vue.html')
 
@@ -94,7 +30,7 @@ def index():
 @app.route('/save', methods=['POST'])
 def save():
     try:
-        #from IPython import embed; embed()
+        # from IPython import embed; embed()
         with open('mydb.txt', 'w') as db:
             print('salvando...')
             json.dump(request.json, db)
@@ -122,4 +58,3 @@ def static_files(path):
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
-#run(host='localhost', port=8080, debug=True)
